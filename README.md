9200fd8965cacfda2044cd53f09767c85c5eded4
igorliotta <liottaigor@gmail.com>
2023-10-18 14:48:01 +0200

    Added: correction

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
 - Da lì parte un timer di 30 secondi. (durante il quale i numero non devono essere visibili all’utente).
   - Creare un setInterval che svolga la mia funzione per la durata di 30 secondi.
   - La funzione da creare dovrà nascondere all'utente l'array di numeri generato in precedenza dentro l'allert.
+
+- Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+- Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
--- js/app.js
+++ js/app.js
@@ -1,28 +1,88 @@
-console.log('Simon Says')
+// console.log('Simon Says')
 
-let arrayRandomNumbers = [];
-console.log(arrayRandomNumbers);
+// let arrayRandomNumbers = [];
+// console.log(arrayRandomNumbers);
 
-for (let i = 0; i < 5; i++) {
+// for (let i = 0; i < 5; i++) {
    
-    const randomNumber = getRandomInt(1, 100);
-    // console.log(randomNumber);
+//     const randomNumber = getRandomInt(1, 100);
+//     // console.log(randomNumber);
 
-    arrayRandomNumbers.push(randomNumber);
+//     arrayRandomNumbers.push(randomNumber);
+// }
+
+// alert(arrayRandomNumbers);
+// console.log(arrayRandomNumbers);
+
+
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// setTimeout(printMyNumbers, 3000);
+
+// function printMyNumbers () {
+//     alert([]);
+// }
+console.log('JS SIMONS');
+
+const numbers = getRandomNumbers(1, 100, 5);
+alert(numbers);
+
+setTimeout(() => {
+
+    const userNumbers = [];
+    
+    for (let i = 0; i < 5; i++) {
+        console.log('Chiedo il numero', i + 1);
+
+        const numero = parseInt(prompt('Inserisci uno dei numeri'));
+        console.log(numero);
+
+        if(!userNumbers.includes(numero)) {
+            userNumbers.push(numero);
+        }
+    }
+
+    console.log(numbers, userNumbers);
+
+    const guessedNumbers = [];
+
+    for (let i = 0; i < userNumbers.length; i++) {
+
+        const currentNumber = userNumbers[i];
+        console.log(numbers.includes(currentNumber), currentNumber);
+
+        if(numbers.includes(currentNumber) && !guessedNumbers.includes(currentNumber)) {
+            guessedNumbers.push(currentNumber);
+        }
+    }
+
+    console.log(guessedNumbers);
+
+    alert('Hai indovinato ' + guessedNumbers.length + ' numeri: ' + guessedNumbers.join(', '));
+}, 3000)
+
+function getRandomNumbers (min, max, num) {
+
+    const numbers = [];
+   
+    while (numbers.length < num) {
+
+        n = getRandomIntInclusive(min, max);
+
+        if (!numbers.includes(n)) {
+            numbers.push(n);
+        }
+    }
+
+    return numbers;
 }
 
-alert(arrayRandomNumbers);
-console.log(arrayRandomNumbers);
-
-
-function getRandomInt(min, max) {
+function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
-}
-
-setInterval(mySaluto, 30000);
-
-function mySaluto () {
-    console.log('Ciaoo');
 }
